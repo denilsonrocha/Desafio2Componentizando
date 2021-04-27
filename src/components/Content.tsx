30da3cc07bbc961073d71d086ce5502b5c3b94ed
Denilson Rocha <denilson.rocha@semantixti.onmicrosoft.com>
2021-04-26 21:53:30 -0300

    componentização do sidebar e content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,30 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Movies {
+  movies: MovieProps[]
+}
+
+export function Content({ movies }: Movies) {
+  return(
+    <>
+       <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+    </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+}
+
+interface Genres{
+  selectedId : number;
+  genres: GenreResponseProps[];
+  onRequestSelect: (id: number) => void;
+}
+
+export function SideBar({selectedId ,genres, onRequestSelect }: Genres) {
+  return(
+    <>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onRequestSelect(genre.id)}
+              selected={selectedId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+    </>
+  );
 }
